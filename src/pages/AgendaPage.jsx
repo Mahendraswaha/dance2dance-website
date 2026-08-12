cab5ab3219cbc5a75b465a69170140c5a59877c4
Mahendraswaha <mahendra.swaha@gmail.com>
2026-08-12 15:50:30 +0200

    agenda corrigindo

--- src/pages/AgendaPage.jsx
+++ src/pages/AgendaPage.jsx
@@ -3,11 +3,13 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import AgendaHub from '../components/AgendaHub';
 
+import programsData from '../data/programs.json';
+
 export default function AgendaPage() {
   return (
     <>
       <Navbar />
-      <AgendaHub />
+      <AgendaHub programs={programsData} />
       <Footer />
     </>
   );
