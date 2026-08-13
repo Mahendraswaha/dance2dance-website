f7c120adc888c0bff4a85a0d33699206aab46181
Mahendraswaha <mahendra.swaha@gmail.com>
2026-08-13 14:30:12 +0200

    voltando

--- src/components/ProgramTemplate.jsx
+++ src/components/ProgramTemplate.jsx
@@ -68,7 +68,7 @@ export default function ProgramTemplate({ program }) {
               <img 
                 src={program.logo} 
                 alt="Program Watermark" 
-                className="h-12 md:h-24 ml-2 md:ml-4 object-contain opacity-60"
+                className="h-12 md:h-24 ml-4 md:ml-10 object-contain opacity-60"
               />
             </div>
           </div>
