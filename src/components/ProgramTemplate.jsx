1f8e44044bb5eed2754e6d83d0e4bee513cc86f7
Mahendraswaha <mahendra.swaha@gmail.com>
2026-08-13 14:07:34 +0200

    ajustando a posição do logo

--- src/components/ProgramTemplate.jsx
+++ src/components/ProgramTemplate.jsx
@@ -68,7 +68,7 @@ export default function ProgramTemplate({ program }) {
               <img 
                 src={program.logo} 
                 alt="Program Watermark" 
-                className="h-12 md:h-24 ml-16 md:ml-64 object-contain opacity-60"
+                className="h-12 md:h-24 ml-12 md:ml-32 object-contain opacity-60"
               />
             </div>
           </div>
