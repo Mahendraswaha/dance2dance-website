43e9a4d4cd8044d7b9e60f5b0006c6f646015ad0
Mahendraswaha <mahendra.swaha@gmail.com>
2026-09-02 23:28:40 +0200

    aulas regulares

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import WorkshopPage from './pages/WorkshopPage';
 import SocialPage from './pages/SocialPage';
 import CorporatePage from './pages/CorporatePage';
 import IndividualPage from './pages/IndividualPage';
+import RegularClassesPage from './pages/RegularClassesPage';
 import ScrollToTop from './components/ScrollToTop';
 
 // Import i18n
@@ -22,6 +23,7 @@ export default function App() {
         <Route path="/social" element={<SocialPage />} />
         <Route path="/biostretch/empresas" element={<CorporatePage />} />
         <Route path="/biostretch/individual" element={<IndividualPage />} />
+        <Route path="/biostretch/aulas-regulares" element={<RegularClassesPage />} />
         <Route path="/:programId" element={<ProgramPage />} />
         <Route path="/:programId/:workshopId" element={<WorkshopPage />} />
       </Routes>
