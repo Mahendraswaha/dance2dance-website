a776ff14306d4e3f2e87cfbd7d4839fcc3e135b7
Mahendraswaha <mahendra.swaha@gmail.com>
2026-09-24 18:40:43 +0200

    speeding up hero

--- src/utils/frameCache.js
+++ src/utils/frameCache.js
@@ -22,20 +22,31 @@ export const preloadFrames = (onProgress) => {
   isPreloading = true;
 
   let loadedCount = 0;
+  
+  // Create the array to hold all images in the correct order
   for (let i = 0; i < frameCount; i++) {
-    const img = new Image();
-    img.src = currentFrame(i);
-    img.onload = () => {
-      loadedCount++;
-      const progress = loadedCount / frameCount;
-      callbacks.forEach(cb => cb(progress));
-      
-      if (loadedCount === frameCount) {
-        isLoaded = true;
-      }
-    };
-    frameCache.push(img);
+    frameCache.push(new Image());
   }
+
+  // Load the FIRST frame immediately (Critical for LCP)
+  frameCache[0].src = currentFrame(0);
+  frameCache[0].onload = () => {
+    loadedCount++;
+    callbacks.forEach(cb => cb(loadedCount / frameCount));
+
+    // Only AFTER frame 1 is loaded (and LCP is satisfied), load the rest
+    for (let i = 1; i < frameCount; i++) {
+      frameCache[i].src = currentFrame(i);
+      frameCache[i].onload = () => {
+        loadedCount++;
+        callbacks.forEach(cb => cb(loadedCount / frameCount));
+        
+        if (loadedCount === frameCount) {
+          isLoaded = true;
+        }
+      };
+    }
+  };
 
   return frameCache;
 };
