35fdefdba1b25f7c4147dd36b0c4adf9f1a5bf02
Mahendraswaha <mahendra.swaha@gmail.com>
2026-09-26 15:12:54 +0200

    Update ImpactPage.jsx

--- src/pages/ImpactPage.jsx
+++ src/pages/ImpactPage.jsx
@@ -119,14 +119,14 @@ const ImpactPage = () => {
 
         {/* Video Layer - Mobile: Below text. Desktop: Original absolute floating position */}
         <div className="relative lg:absolute lg:top-[53%] left-0 w-full px-0 lg:px-12 h-auto lg:h-[70vh] lg:-translate-y-1/2 z-0 pointer-events-none mt-8 lg:mt-0">
-          <div className="w-full max-w-5xl mx-auto h-full relative md:translate-x-1">
+          <div className="w-full max-w-5xl mx-auto h-full relative overflow-hidden md:overflow-visible md:translate-x-1">
             <video 
               autoPlay 
               loop 
               muted 
               playsInline
               poster={heroBlurPlaceholder}
-              className="w-full h-auto lg:h-full object-contain object-center lg:object-right opacity-60 mix-blend-luminosity"
+              className="w-[135%] max-w-[135%] -ml-[35%] lg:w-full lg:max-w-none lg:ml-0 h-auto lg:h-full object-contain object-center lg:object-right opacity-60 mix-blend-luminosity"
             >
               <source src="/hero-social-project-small.mp4" type="video/mp4" />
             </video>
@@ -384,6 +384,8 @@ const ImpactPage = () => {
 };
 
 export default ImpactPage;
+
+
 
 
 
